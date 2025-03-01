4b108168a03929630e8de515c6363f03ad9c85a9
Mohamed Imghri <mohamedimghri06@gmail.com>
2025-03-01 13:02:10 +0000

    grocery_items modified

--- lib/components/grocery_item_tile.dart
+++ lib/components/grocery_item_tile.dart
@@ -5,18 +5,47 @@ class GroceryItemTile extends StatelessWidget {
   final String itemPrice;
   final String imagePath;
   final color;
+  void Function()? onPressed;
 
-  const GroceryItemTile({super.key,
-    required this.itemName,
-    required this.itemPrice,
-    required this.imagePath,
-    required this.color
-  
-  
-  });
+  GroceryItemTile(
+      {super.key,
+      required this.itemName,
+      required this.itemPrice,
+      required this.imagePath,
+      required this.color,
+      this.onPressed
+      });
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: color[100],
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Image.asset(
+            imagePath,
+            height: 64,
+          ),
+          Text(itemName),
+          MaterialButton(
+            onPressed: onPressed,
+            color: color[800],
+            child: Text(
+              '\$$itemPrice',
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
+        ]),
+      ),
+    );
   }
 }
